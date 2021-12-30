98ae4bd02003fb29b75eae57443e0085901f1c47
Someshbose <iamsomeshbose@gmail.com>
2021-12-31 00:13:59 +0530

    2048 game

---
+++ 2048Game/src/main/java/main/Cell.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Cell {
+  private int x;
+  private int y;
+  public Cell(int x,int y){
+    this.x=x;
+    this.y=y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+}
