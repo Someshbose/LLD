00ae3df09e36e0327c2c40410c528261a1a80691
Somesh Bose <smartsomubose635@gmail.com>
2022-01-11 11:03:30 +0530

    2048 game (#13)
    * 2048 game
    
    * 2048 game changes

---
+++ 2048Game/src/main/java/main/Computer.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.security.SecureRandom;
+import java.util.List;
+
+public class Computer {
+//private static final Logger LOG = LoggerFactory.getLogger(Computer.class);
+
+private final SecureRandom rng = new SecureRandom();
+
+public Board makeMove(Board input) {
+    List<Cell> emptyCells = input.emptyCells();
+    //LOG.info("Number of empty cells: {}", emptyCells.size());
+
+    double numberToPlace = rng.nextDouble();
+   // LOG.info("New number probability: {}", numberToPlace);
+
+    int indexToPlace = rng.nextInt(emptyCells.size());
+    Cell cellToPlace = emptyCells.get(indexToPlace);
+   // LOG.info("Placing number into empty cell: {}", cellToPlace);
+
+    return input.placeTile(cellToPlace, numberToPlace >= 0.9 ? 4 : 2);
+    }
+    }
